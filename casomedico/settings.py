7a4154cdd55b78c9d16e9a9313a025cc088a0ff6
Nicolás <nicolasrenier.v@gmail.com>
2022-11-26 21:28:22 -0300

    database changet

--- casomedico/settings.py
+++ casomedico/settings.py
@@ -82,16 +82,7 @@ WSGI_APPLICATION = 'casomedico.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': { dj_database_url.config(
-        default='postgresql://postgres:posgres@localhost/postgres',
-        conn_max_age=600
-    )
-
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        
-    }
-}
+    'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/mysite', conn_max_age=600)}
 
 
 # Password validation

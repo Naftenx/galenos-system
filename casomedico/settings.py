0934a88570f61b6bdbc963f3e855f9b2b42e7e9d
Nicolás <nicolasrenier.v@gmail.com>
2022-11-28 23:49:53 -0300

    second commit

--- casomedico/settings.py
+++ casomedico/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')

4ea742fe8904c05edc990b1911895a8d34b569a6
Nicolás <nicolasrenier.v@gmail.com>
2022-11-29 00:46:40 -0300

    build sh rollback

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 #poetry install
 pip install -r requirements.txt
 
-python ./django-crud-auth/manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 python manage.py migrate
 
 
